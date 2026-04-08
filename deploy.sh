1cc2643543c77b71b85cb7c29306ed1e433ded5e
woodykim-dot <woody.kim@gsenergy.in>
2026-04-08 09:48:17 +0900

    modified

--- deploy.sh
+++ deploy.sh
@@ -17,6 +17,7 @@ VERSION="fix/multi-region-waf-s3-naming"
 STACK_NAME="sample-aws-idp-pipeline-codebuild"
 TEMPLATE_URL_BASE="https://raw.githubusercontent.com/aws-samples/sample-aws-idp-pipeline"
 TEMPLATE_FILE="/tmp/deploy-codebuild.yml"
+TEMPLATE_VERSION="main"
 ADMIN_USER_EMAIL=""
 DEPLOY_STACKS=""
 
@@ -98,7 +99,7 @@ while true; do
 done
 
 # Download CloudFormation template
-TEMPLATE_URL="${TEMPLATE_URL_BASE}/${VERSION}/deploy-codebuild.yml"
+TEMPLATE_URL="${TEMPLATE_URL_BASE}/${TEMPLATE_VERSION}/deploy-codebuild.yml"
 echo ""
 echo "Downloading CloudFormation template..."
 echo "  $TEMPLATE_URL"

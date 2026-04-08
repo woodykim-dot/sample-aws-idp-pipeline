b3c1c2cdfca3169ce6d021229d3a8a1a5b05152f
woodykim-dot <woody.kim@gsenergy.in>
2026-04-08 09:43:21 +0900

    Changed version in deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -13,7 +13,7 @@ echo ""
 
 # Default parameters
 REPO_URL="https://github.com/aws-samples/sample-aws-idp-pipeline.git"
-VERSION="main"
+VERSION="fix/multi-region-waf-s3-naming"
 STACK_NAME="sample-aws-idp-pipeline-codebuild"
 TEMPLATE_URL_BASE="https://raw.githubusercontent.com/aws-samples/sample-aws-idp-pipeline"
 TEMPLATE_FILE="/tmp/deploy-codebuild.yml"
